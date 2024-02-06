431e3d0338d9d2eb1b855118d614c04167ff319b
leothomson23 <146092733+leothomson23@users.noreply.github.com>
2024-02-06 09:48:00 +0000

    Uploading files from Week 1 tasks.

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,12 @@
+
+public class PersonalDetails {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Leo Thomson");
+		System.out.println("Age: 18");
+		System.out.println("University: Leeds Beckett Univerity");
+		System.out.println("Phone Number: 07717845568");
+
+	}
+
+}
